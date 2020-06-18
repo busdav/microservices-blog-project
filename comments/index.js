8dcfadd253fb9abac58a37e098d036588a6eb794
David Buser <busdav@outlook.com>
2020-06-18 11:12:56 +0100

    Final project

---
+++ comments/index.js
@@ -0,0 +1,72 @@
+const express = require("express");
+const { randomBytes } = require("crypto");
+const cors = require("cors");
+const axios = require("axios");
+
+const app = express();
+app.use(express.json({ extended: false }));
+app.use(cors());
+
+const commentsByPostId = {};
+
+app.get("/posts/:id/comments", (req, res) => {
+  res.send(commentsByPostId[req.params.id] || []);
+});
+
+app.post("/posts/:id/comments", async (req, res) => {
+  const commentId = randomBytes(4).toString("hex");
+  const { content } = req.body;
+  /* 
+  If there is no comments array yet for a given post (i.e. first value below is `undefined`), 
+  then give by default an empty array 
+  */
+  const comments = commentsByPostId[req.params.id] || [];
+
+  comments.push({ id: commentId, content, status: "pending" });
+
+  commentsByPostId[req.params.id] = comments;
+
+  await axios.post("http://event-bus-srv:4005/events", {
+    type: "CommentCreated",
+    data: {
+      id: commentId,
+      content,
+      postId: req.params.id,
+      status: "pending",
+    },
+  });
+
+  res.status(201).send(comments);
+});
+
+app.post("/events", async (req, res) => {
+  console.log("Event Received:", req.body.type);
+
+  const { type, data } = req.body;
+
+  if (type === "CommentModerated") {
+    const { postId, id, status, content } = data;
+    const comments = commentsByPostId[postId];
+
+    const comment = comments.find((comm) => {
+      return comm.id === id;
+    });
+    comment.status = status;
+
+    await axios.post("http://event-bus-srv:4005/events", {
+      type: "CommentUpdated",
+      data: {
+        id,
+        status,
+        postId,
+        content,
+      },
+    });
+  }
+
+  res.send({});
+});
+
+app.listen(4001, () => {
+  console.log("Listening on 4001");
+});
---
+++ query/index.js
@@ -0,0 +1,69 @@
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
+
+const app = express();
+app.use(express.json({ extended: false }));
+app.use(cors());
+
+const posts = {};
+
+const handleEvent = (type, data) => {
+  if (type === "PostCreated") {
+    const { id, title } = data;
+
+    posts[id] = { id, title, comments: [] };
+  }
+
+  if (type === "CommentCreated") {
+    const { id, content, postId, status } = data;
+
+    const post = posts[postId];
+
+    post.comments.push({ id, content, status });
+  }
+
+  if (type === "CommentUpdated") {
+    const { id, content, postId, status } = data;
+
+    const post = posts[postId];
+    const comment = post.comments.find((comm) => {
+      return comm.id === id;
+    });
+
+    comment.status = status;
+    comment.content = content;
+  }
+};
+
+app.get("/posts", async (req, res) => {
+  res.send(posts);
+});
+
+app.post("/events", (req, res) => {
+  const { type, data } = req.body;
+
+  handleEvent(type, data);
+
+  res.send({});
+});
+
+app.listen(4002, async () => {
+  console.log("Listening on port 4002");
+
+  const res = await axios.get("http://event-bus-srv:4005/events");
+
+  // Whenever we use axios, the actual data we get back is available on the `data` property of the response object
+  for (let event of res.data) {
+    console.log('Processing event:', event.type);
+
+    handleEvent(event.type, event.data);
+  }
+  
+  // Alternative: (but had some difficulties with this inside app.listen):
+  // res.data.forEach((event) => {
+  //   console.log("Processing event:", event.type);
+
+  //   handleEvent(event.type, event.data);
+  // });
+});
